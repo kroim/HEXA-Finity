7229f524118e25023e8ef4a8d6b4481247935adc
TuanThai <donaldjay1030@gmail.com>
2022-12-01 15:29:20 -0800

    Updated to get farms in homepage

--- apps/web/src/views/Home/components/FarmsPoolsTables/index.tsx
+++ apps/web/src/views/Home/components/FarmsPoolsTables/index.tsx
@@ -21,6 +21,7 @@ import BigNumber from 'bignumber.js'
 import { FarmWithStakedValue } from './types'
 
 import Table from './Farms/components/FarmTable/FarmTable'
+import Farms from './Farms/Farms'
 import CollectWinningsOverlay from 'views/Predictions/components/RoundCard/CollectWinningsOverlay'
 
 const MainArea = styled.div`
@@ -150,115 +151,108 @@ const FarmsPoolsTables: React.FC<React.PropsWithChildren> = () => {
 
   const [stakedOnly, setStakedOnly] = useUserFarmStakedOnly(isActive)
 
-  useCakeVaultUserData()
-  usePollFarmsWithUserData()
-
   console.log('_____________farmsLP111', farmsLP)
-  const activeFarms = farmsLP.filter(
-    (farm) => farm.pid !== 0 && farm.multiplier !== '0X' && (!poolLength || poolLength > farm.pid),
-  )
-  const inactiveFarms = farmsLP.filter((farm) => farm.pid !== 0 && farm.multiplier === '0X')
-  const archivedFarms = farmsLP
+  // const activeFarms = farmsLP.filter(
+  //   (farm) => farm.pid !== 0 && farm.multiplier !== '0X' && (!poolLength || poolLength > farm.pid),
+  // )
+  // const inactiveFarms = farmsLP.filter((farm) => farm.pid !== 0 && farm.multiplier === '0X')
+  // const archivedFarms = farmsLP
 
-  const stakedOnlyFarms = activeFarms.filter(
-    (farm) =>
-      farm.userData &&
-      (new BigNumber(farm.userData.stakedBalance).isGreaterThan(0) ||
-        new BigNumber(farm.userData.proxy?.stakedBalance).isGreaterThan(0)),
-  )
+  // const stakedOnlyFarms = activeFarms.filter(
+  //   (farm) =>
+  //     farm.userData &&
+  //     (new BigNumber(farm.userData.stakedBalance).isGreaterThan(0) ||
+  //       new BigNumber(farm.userData.proxy?.stakedBalance).isGreaterThan(0)),
+  // )
 
-  const stakedInactiveFarms = inactiveFarms.filter(
-    (farm) =>
-      farm.userData &&
-      (new BigNumber(farm.userData.stakedBalance).isGreaterThan(0) ||
-        new BigNumber(farm.userData.proxy?.stakedBalance).isGreaterThan(0)),
-  )
+  // const stakedInactiveFarms = inactiveFarms.filter(
+  //   (farm) =>
+  //     farm.userData &&
+  //     (new BigNumber(farm.userData.stakedBalance).isGreaterThan(0) ||
+  //       new BigNumber(farm.userData.proxy?.stakedBalance).isGreaterThan(0)),
+  // )
 
-  const stakedArchivedFarms = archivedFarms.filter(
-    (farm) =>
-      farm.userData &&
-      (new BigNumber(farm.userData.stakedBalance).isGreaterThan(0) ||
-        new BigNumber(farm.userData.proxy?.stakedBalance).isGreaterThan(0)),
-  )
+  // const stakedArchivedFarms = archivedFarms.filter(
+  //   (farm) =>
+  //     farm.userData &&
+  //     (new BigNumber(farm.userData.stakedBalance).isGreaterThan(0) ||
+  //       new BigNumber(farm.userData.proxy?.stakedBalance).isGreaterThan(0)),
+  // )
 
-  const farmsList = useCallback(
-    (farmsToDisplay: DeserializedFarm[]): FarmWithStakedValue[] => {
-      console.log('___________________farmsToDisplay111', farmsToDisplay)
-      let farmsToDisplayWithAPR: FarmWithStakedValue[] = farmsToDisplay.map((farm) => {
-        if (!farm.lpTotalInQuoteToken || !farm.quoteTokenPriceBusd) {
-          return farm
-        }
+  // const farmsList = useCallback(
+  //   (farmsToDisplay: DeserializedFarm[]): FarmWithStakedValue[] => {
+  //     console.log('___________________farmsToDisplay111', farmsToDisplay)
+  //     let farmsToDisplayWithAPR: FarmWithStakedValue[] = farmsToDisplay.map((farm) => {
+  //       if (!farm.lpTotalInQuoteToken || !farm.quoteTokenPriceBusd) {
+  //         return farm
+  //       }
 
-        const totalLiquidity = new BigNumber(farm.lpTotalInQuoteToken).times(farm.quoteTokenPriceBusd)
-        const { cakeRewardsApr, lpRewardsApr } = isActive
-          ? getFarmApr(
-              chainId,
-              new BigNumber(farm.poolWeight),
-              cakePrice,
-              totalLiquidity,
-              farm.lpAddress,
-              regularCakePerBlock,
-            )
-          : { cakeRewardsApr: 0, lpRewardsApr: 0 }
+  //       const totalLiquidity = new BigNumber(farm.lpTotalInQuoteToken).times(farm.quoteTokenPriceBusd)
+  //       const { cakeRewardsApr, lpRewardsApr } = isActive
+  //         ? getFarmApr(
+  //             chainId,
+  //             new BigNumber(farm.poolWeight),
+  //             cakePrice,
+  //             totalLiquidity,
+  //             farm.lpAddress,
+  //             regularCakePerBlock,
+  //           )
+  //         : { cakeRewardsApr: 0, lpRewardsApr: 0 }
 
-        return { ...farm, apr: cakeRewardsApr, lpRewardsApr, liquidity: totalLiquidity }
-      })
+  //       return { ...farm, apr: cakeRewardsApr, lpRewardsApr, liquidity: totalLiquidity }
+  //     })
 
-      console.log('______________farmsToDisplayWithAPR', farmsToDisplayWithAPR)
-      return farmsToDisplayWithAPR
-    },
-    [chainId, cakePrice, regularCakePerBlock],
-  )
+  //     console.log('______________farmsToDisplayWithAPR', farmsToDisplayWithAPR)
+  //     return farmsToDisplayWithAPR
+  //   },
+  //   [chainId, cakePrice, regularCakePerBlock],
+  // )
 
-  const chosenFarms: FarmWithStakedValue[] = useMemo(() => {
-    let chosenFs: FarmWithStakedValue[] = []
-    if (isActive) {
-      chosenFs = stakedOnly ? farmsList(stakedOnlyFarms) : farmsList(activeFarms)
-    }
-    if (isInactive) {
-      chosenFs = stakedOnly ? farmsList(stakedInactiveFarms) : farmsList(inactiveFarms)
-    }
-    if (isArchived) {
-      chosenFs = stakedOnly ? farmsList(stakedArchivedFarms) : farmsList(archivedFarms)
-    }
-    console.log('________________', chosenFs);
-    return chosenFs
-  }, [
-    farmsList,
+  // const chosenFarms: FarmWithStakedValue[] = useMemo(() => {
+  //   let chosenFs: FarmWithStakedValue[] = []
+  //   if (isActive) {
+  //     chosenFs = stakedOnly ? farmsList(stakedOnlyFarms) : farmsList(activeFarms)
+  //   }
+  //   if (isInactive) {
+  //     chosenFs = stakedOnly ? farmsList(stakedInactiveFarms) : farmsList(inactiveFarms)
+  //   }
+  //   if (isArchived) {
+  //     chosenFs = stakedOnly ? farmsList(stakedArchivedFarms) : farmsList(archivedFarms)
+  //   }
+  //   console.log('________________', chosenFs);
+  //   return chosenFs
+  // }, [
+  //   farmsList,
 
-  ])
+  // ])
 
   return (
-    <FarmsContext.Provider value={{ chosenFarms }}>
-      <Flex justifyContent="center" alignItems="center" flexDirection="column" padding="0px">
-      <MainArea>
-        <ItemArea>
-          <Table farms={chosenFarms} cakePrice={cakePrice} userDataReady={userDataReady} />
-          {account && !userDataLoaded && stakedOnly && (
-            <Flex justifyContent="center">
-              <Loading />
-            </Flex>
-          )}
-        </ItemArea>
-        <ItemArea style={{ backgroundColor: '#2F4DA0' }}>
-          <RightIteam>
-            <img src="/images/binanceImage.png" alt="lastesFeeImage" />
-            <div>
-              <p>Total Value Locked</p>
-              <h2>$948 561 357</h2>
-            </div>
-          </RightIteam>
-          <RightIteam>
-            <div>
-              <p>Total Trading Volume</p>
-              <h2>$47721756347</h2>
-            </div>
-          </RightIteam>
-        </ItemArea>
-      </MainArea>
-    </Flex>
-    </FarmsContext.Provider>
-    
+    <MainArea>
+      <ItemArea>
+        {/* <Table farms={chosenFarms} cakePrice={cakePrice} userDataReady={userDataReady} /> */}
+        <Farms />
+        {account && !userDataLoaded && stakedOnly && (
+          <Flex justifyContent="center">
+            <Loading />
+          </Flex>
+        )}
+      </ItemArea>
+      <ItemArea style={{ backgroundColor: '#2F4DA0' }}>
+        <RightIteam>
+          <img src="/images/binanceImage.png" alt="lastesFeeImage" />
+          <div>
+            <p>Total Value Locked</p>
+            <h2>$948 561 357</h2>
+          </div>
+        </RightIteam>
+        <RightIteam>
+          <div>
+            <p>Total Trading Volume</p>
+            <h2>$47721756347</h2>
+          </div>
+        </RightIteam>
+      </ItemArea>
+    </MainArea>
   )
 }
 
