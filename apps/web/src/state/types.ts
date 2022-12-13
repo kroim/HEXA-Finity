5e4ad8b76c317c1cedeb451ada767ce15f553412
devsmith0213 <devsmith0213@gmail.com>
2022-12-13 01:12:49 -0500

    fixed bignumber issue for add page

--- apps/web/src/state/types.ts
+++ apps/web/src/state/types.ts
@@ -17,8 +17,8 @@ export enum GAS_PRICE {
 
 export const GAS_PRICE_GWEI = {
   rpcDefault: 'rpcDefault',
-  // default: parseUnits(GAS_PRICE.default, 'gwei').toString(),
-  default: 'default',
+  default: parseUnits(GAS_PRICE.default, 'gwei').toString(),
+  // default: 'default',
   fast: parseUnits(GAS_PRICE.fast, 'gwei').toString(),
   instant: parseUnits(GAS_PRICE.instant, 'gwei').toString(),
   testnet: parseUnits(GAS_PRICE.testnet, 'gwei').toString(),
